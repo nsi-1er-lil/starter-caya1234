e4224ee9d3d38a6f8fa256f4e6d7319f915223ee
caya1234 <cayaa@lyceechurchill.london>
2024-09-24 14:36:27 +0000

    exo 6-8

--- Debuter avec Python/exercice_4.py
+++ Debuter avec Python/exercice_4.py
@@ -1,7 +1,15 @@
-c = float(imput))
+c = 10
 P = 4 * c
 A = c ** 2
 b = (A > 5)
 print(P)
 print(A)
 print(b)
+
+def perimetre(c):
+    return(c*4)
+print(perimetre(c))
+
+def surface(c):
+    return(c**2)
+print(surface(c))
